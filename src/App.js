a9233f1a7cc8f93ec30baa487df653e3ad3b4e1f
RaviKant <rk3727000@gmail.com>
2024-04-07 13:23:42 +0545

    Adding Code-Base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-300 h-screen flex items-center justify-center text-6xl font-extrabold text-purple-600 font-serif content-center">
+      Welcome To <span className="ml-2 text-8xl text-green-600">UNION</span>
+    </div>
+  );
+}
+
+export default App;
