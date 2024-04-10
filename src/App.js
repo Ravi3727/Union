ceabcad12ce0103e45f3c5bb61d4abdd1b85b0f5
RaviKant <rk3727000@gmail.com>
2024-04-10 12:04:31 +0545

    adding Images an Video Feed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import HeroSection from './components/HeroSection';
 import HeroCardSection from './components/HeroCardSection';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
+import VideoFeedCard from './components/VideoFeedCard';
 AOS.init();
 
 
 function App() {
   return <>
-    <div className='h-full w-full bg-blue-400'>
+    <div className='h-full w-full bg-blue-400 select-none flex flex-col gap-3'>
       <div className='h-screen w-8/12 flex justify-center items-center mx-auto bg-blue-400'>
         <HeroSection />
       </div>
       <div className='w-11/12 ml-16 mt-3'>
 
         <HeroCardSection />
+      </div>
+      <div className='w-11/12 ml-16 mt-3'>
+
+        <VideoFeedCard />
       </div>
     </div>
   </>
