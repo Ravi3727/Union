c0c0cf6626a82dc17cd8bca6eb1028b61fed2071
Ansh <ansh03012004@gmail.com>
2024-04-12 12:09:06 +0530

    Post Form

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NewPostForm from "../components/creator/NewPostForm";
+// import Login from "../components/Auth/login";
+// import Signup from "../components/Auth/signup";
+
+const HomePage = () => {
+  return <NewPostForm />;
+};
+
+export default HomePage;
